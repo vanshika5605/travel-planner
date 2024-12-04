87999ea989e9ea33b9f9d5cb5b15267f60511197
Mustafa-Ali-Atica <mustafa@aticastays.com>
2024-12-04 18:40:35 -0500

    New APIs added

--- backend/travelPlanner/src/main/java/com/dgoil/travelPlanner/Service/packingListService.java
+++ backend/travelPlanner/src/main/java/com/dgoil/travelPlanner/Service/packingListService.java
@@ -1,16 +1,34 @@
 package com.dgoil.travelPlanner.Service;
 
 import com.dgoil.travelPlanner.Model.DAO.PackingList;
+import com.dgoil.travelPlanner.Model.DAO.UserItinerary;
 import com.dgoil.travelPlanner.Repository.packingListRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class packingListService {
     @Autowired
     packingListRepo mypackingListRepo;
+    @Autowired
+    userItineraryService userItineraryService;
 
     public void saveList(PackingList packingList){
+
+        Optional<UserItinerary> userItinerary = userItineraryService.getUserItinerary(packingList.getTripID());
+        if(userItinerary.isPresent()) {
+            UserItinerary userItineraryData = userItinerary.get();
+            userItineraryData.setIsPackingListCreated(Boolean.TRUE);
+            userItineraryService.saveUserItinerary(userItineraryData);
+        }
         mypackingListRepo.save(packingList);
     }
+
+     public PackingList getList(String tripId) {
+         return mypackingListRepo.getByTripId(tripId)
+                 .orElseThrow(() -> new IllegalArgumentException("Packing list not found for TripID: " + tripId));
+     }
 }
+
