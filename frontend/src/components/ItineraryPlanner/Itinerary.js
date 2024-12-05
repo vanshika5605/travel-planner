c089ca31fd927e8eadac0fc074466d510d6535d5
Vanshika Agrawal <vanshikaagrawal56@gmail.com>
2024-12-04 22:34:28 -0500

    Styling changes for itinerary page

--- frontend/src/components/ItineraryPlanner/Itinerary.js
+++ frontend/src/components/ItineraryPlanner/Itinerary.js
@@ -1,45 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlane,
-  faUtensils,
-  faBed,
-  faHiking,
-  faCartShopping,
-  faVanShuttle
-} from "@fortawesome/free-solid-svg-icons";
-import "./Itinerary.css";
-import TripDetailsBox from "./TripDetailsBox";
 import backend from "../Utils/backend";
-
-// Mapping categories to icons
-const categoryIcons = {
-  Travel: faPlane,
-  Adventure: faHiking,
-  Food: faUtensils,
-  Hotel: faBed,
-  Shopping: faCartShopping,
-  Sightseeing: faVanShuttle,
-};
+import "./Itinerary.css";
+import ItineraryBox from "./ItineraryBox";
+import TripDetailsBox from "./TripDetailsBox";
 
 const Itinerary = () => {
   const location = useLocation();
-  
+
   // Extract data from location state with fallback
-  const { tripData, userId, itineraryData: initialItineraryData } = location.state || {
+  const {
+    tripData,
+    userId,
+    itineraryData: initialItineraryData,
+  } = location.state || {
     tripData: {},
-    userId: '',
+    userId: "",
     itineraryData: {
       summary: "",
       itinerary: [],
-      note: ""
-    }
+      note: "",
+    },
   };
 
   const [itineraryData, setItineraryData] = useState(initialItineraryData);
-  const [activeTab, setActiveTab] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
 
   // Ensure we have at least one day when component loads
@@ -47,37 +31,18 @@ const Itinerary = () => {
     if (!itineraryData.itinerary || itineraryData.itinerary.length === 0) {
       setItineraryData({
         summary: "",
-        itinerary: [{
-          date: new Date().toISOString().split('T')[0],
-          day: "Day 1",
-          activities: [],
-          note: ""
-        }],
-        note: ""
+        itinerary: [
+          {
+            date: new Date().toISOString().split("T")[0],
+            day: "Day 1",
+            activities: [],
+            note: "",
+          },
+        ],
+        note: "",
       });
     }
   }, []);
-
-  // Handle drag and drop
-  const handleDragEnd = (result, dayIndex) => {
-    if (!result.destination) return;
-
-    const items = Array.from(itineraryData.itinerary[dayIndex].activities);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    const newItinerary = [...itineraryData.itinerary];
-    newItinerary[dayIndex].activities = items;
-
-    setItineraryData({ ...itineraryData, itinerary: newItinerary });
-  };
-
-  // Handle note change for each day
-  const handleNoteChange = (dayIndex, value) => {
-    const newItinerary = [...itineraryData.itinerary];
-    newItinerary[dayIndex].note = value;
-    setItineraryData({ ...itineraryData, itinerary: newItinerary });
-  };
 
   // Copy to clipboard
   const copyToClipboard = () => {
@@ -108,12 +73,12 @@ const Itinerary = () => {
   };
 
   const saveTrip = async () => {
-    let tripDataNew = { 
-      ...tripData, 
-      email: userId, 
-      tripDetails: itineraryData 
+    let tripDataNew = {
+      ...tripData,
+      email: userId,
+      tripDetails: itineraryData,
     };
-    
+
     try {
       const response = await backend.saveTrip(tripDataNew);
       if (response.status === 200) {
@@ -146,87 +111,21 @@ const Itinerary = () => {
           {errorMessage}
         </div>
       )}
-      <h1>Itinerary</h1>
 
       <div className="trip-itinerary">
         <TripDetailsBox tripDetails={tripData}></TripDetailsBox>
-        <div>
-          <div className="itinerary-container">
-            {/* Tabs for each day */}
-            <div className="tabs">
-              {itineraryData.itinerary.map((day, index) => (
-                <div
-                  key={day.date}
-                  onClick={() => setActiveTab(index)}
-                  className={`tab ${activeTab === index ? "active" : ""}`}
-                >
-                  {day.day}
-                </div>
-              ))}
-            </div>
-
-            {/* Display active day's activities */}
-            <div className="day-container">
-              <h3>{itineraryData.itinerary[activeTab].date}</h3>
-
-              <DragDropContext
-                onDragEnd={(result) => handleDragEnd(result, activeTab)}
-              >
-                <Droppable droppableId={`day-${activeTab}`}>
-                  {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                      {itineraryData.itinerary[activeTab].activities.map(
-                        (activity, index) => (
-                          <Draggable
-                            key={`${activity.activity}-${index}`}
-                            draggableId={`${activity.activity}-${index}`}
-                            index={index}
-                          >
-                            {(provided) => (
-                              <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                className="activity-item"
-                                style={{
-                                  ...provided.draggableProps.style,
-                                }}
-                              >
-                                <div className="activity-content">
-                                  <FontAwesomeIcon
-                                    icon={categoryIcons[activity.category]}
-                                    className="category-icon"
-                                  />
-                                  <span>{activity.activity}</span>
-                                </div>
-                              </div>
-                            )}
-                          </Draggable>
-                        )
-                      )}
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-
-              {/* Text area for notes */}
-              <textarea
-                placeholder="Add notes for this day..."
-                value={itineraryData.itinerary[activeTab].note}
-                onChange={(e) => handleNoteChange(activeTab, e.target.value)}
-                className="note-textarea"
-              />
-            </div>
-
-            {/* Button to copy itinerary */}
-            <button onClick={copyToClipboard} className="copy-button">
-              Copy Itinerary to Clipboard
-            </button>
-            <button onClick={saveTrip} className="copy-button">
-              Save Trip
-            </button>
-          </div>
+        <div className="itinerary-details-box">
+          <ItineraryBox
+            itineraryData={itineraryData}
+            setItineraryData={setItineraryData}
+          ></ItineraryBox>
+          {/* Button to copy itinerary */}
+          <button onClick={copyToClipboard} className="copy-button">
+            Copy Itinerary to Clipboard
+          </button>
+          <button onClick={saveTrip} className="copy-button">
+            Save Trip
+          </button>
         </div>
       </div>
     </>
---
+++ frontend/src/components/ItineraryPlanner/ItineraryBox.js
@@ -0,0 +1,121 @@
+import {
+  faBed,
+  faCartShopping,
+  faHiking,
+  faPlane,
+  faUtensils,
+  faVanShuttle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./Itinerary.css";
+
+// Mapping categories to icons
+const categoryIcons = {
+  Travel: faPlane,
+  Adventure: faHiking,
+  Food: faUtensils,
+  Hotel: faBed,
+  Shopping: faCartShopping,
+  Sightseeing: faVanShuttle,
+};
+
+const ItineraryBox = ({ itineraryData, setItineraryData }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleDragEnd = (result, dayIndex) => {
+    if (!result.destination) return;
+
+    const items = Array.from(itineraryData.itinerary[dayIndex].activities);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    const newItinerary = [...itineraryData.itinerary];
+    newItinerary[dayIndex].activities = items;
+
+    setItineraryData({ ...itineraryData, itinerary: newItinerary });
+  };
+
+  // Handle note change for each day
+  const handleNoteChange = (dayIndex, value) => {
+    const newItinerary = [...itineraryData.itinerary];
+    newItinerary[dayIndex].note = value;
+    setItineraryData({ ...itineraryData, itinerary: newItinerary });
+  };
+
+  return (
+    <div className="itinerary-container">
+              <h2>Itinerary</h2>
+
+      {/* Tabs for each day */}
+      <div className="tabs">
+        {itineraryData.itinerary.map((day, index) => (
+          <div
+            key={day.date}
+            onClick={() => setActiveTab(index)}
+            className={`tab ${activeTab === index ? "active" : ""}`}
+          >
+            {day.day}
+          </div>
+        ))}
+      </div>
+
+      {/* Display active day's activities */}
+      <div className="day-container">
+        <h4>{itineraryData.itinerary[activeTab].date}</h4>
+
+        <DragDropContext
+          onDragEnd={(result) => handleDragEnd(result, activeTab)}
+        >
+          <Droppable droppableId={`day-${activeTab}`}>
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {itineraryData.itinerary[activeTab].activities.map(
+                  (activity, index) => (
+                    <Draggable
+                      key={`${activity.activity}-${index}`}
+                      draggableId={`${activity.activity}-${index}`}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="activity-item"
+                          style={{
+                            ...provided.draggableProps.style,
+                          }}
+                        >
+                          <div className="activity-content">
+                            <FontAwesomeIcon
+                              icon={categoryIcons[activity.category]}
+                              className="category-icon"
+                            />
+                            <span>{activity.activity}</span>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                )}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+
+        {/* Text area for notes */}
+        <textarea
+          placeholder="Add notes for this day..."
+          value={itineraryData.itinerary[activeTab].note}
+          onChange={(e) => handleNoteChange(activeTab, e.target.value)}
+          className="note-textarea"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ItineraryBox;
