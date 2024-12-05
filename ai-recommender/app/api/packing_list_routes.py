31f42a3d46c2fac142a849b6a94409218abb8292
anusha2211 <anushaagrawal67@gmail.com>
2024-12-05 02:37:36 -0400

    added api url and updated according to packing list

--- ai-recommender/app/api/packing_list_routes.py
+++ ai-recommender/app/api/packing_list_routes.py
@@ -1,14 +1,14 @@
 # app/api/itinerary_routes.py
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
-from ..services.itinerary_service import ItineraryService
-from ..utils.exceptions import ItineraryGenerationError
+from ..services.itinerary_service import PackingListService
+from ..utils.exceptions import PackingListGenerationError
 
 packing_list_blueprint = Blueprint('packing-list', __name__, url_prefix='/api/v1/packingList')
 
 @packing_list_blueprint.route('/create', methods=['POST'])
 @cross_origin()
-def generate_itinerary(itinerary_service: ItineraryService):
+def generate_packing_list(packing_list_service: PackingListService):
     """
     API endpoint to generate a travel itinerary.
     
@@ -17,14 +17,17 @@ def generate_itinerary(itinerary_service: ItineraryService):
     """
     try:
         # Validate input
-        if not request.json:
-            return jsonify({"error": "No input data provided"}), 400
+        if not request.json or "api_url" not in request.json:
+            return jsonify({"error": "API URL not provided"}), 400
+            # return jsonify({"error": "No input data provided"}), 400
+        
+        api_url = request.json["api_url"]
 
         # Generate itinerary
-        itinerary = itinerary_service.generate_itinerary(request.json)
-        return jsonify({"itinerary": itinerary})
+        packing_list = packing_list_service.generate_packing_list(api_url)
+        return jsonify({"packingList": packing_list})
 
-    except ItineraryGenerationError as e:
+    except PackingListGenerationError as e:
         return jsonify({"error": str(e)}), 500
     except Exception as e:
         return jsonify({"error": "Unexpected error occurred"}), 500
