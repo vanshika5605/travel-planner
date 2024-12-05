a4c692b1a1eb39a08d14a5cc54a7e800a45e79aa
Devyani Goil <goildevyani1@gmail.com>
2024-12-05 01:30:08 -0500

    Footer Changes

--- frontend/src/components/Utils/Footer.js
+++ frontend/src/components/Utils/Footer.js
@@ -14,12 +14,13 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Location</h3>
           {/* <p>123 Demo Street</p> */}
-          <p>Amherst, MA - 01002</p>
+          <p>Amherst</p>
+          <p>MA - 01002</p>
         </div>
         <div className="footer-section">
           <h3>Contact</h3>
           <p><a href="mailto:email@example.com">contact@travelbuddy.com</a></p>
-          <p>(555) 555-5555</p>
+          <p>(123) 456-7890</p>
         </div>
       </div>
     </footer>
